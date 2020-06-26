06719f69ce08415e76874e1bd7bdc6b2424f9737
xdoo <claus.straube@gmail.com>
2020-06-26 08:22:48 +0200

    added todo button

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -19,42 +19,6 @@
       <v-icon>mdi-map-marker-plus</v-icon>
     </v-btn>
   </v-container>
-<!--  <dummy-map2-->
-<!--        height="100%"-->
-<!--        width="100%"-->
-<!--        show-marker=true-->
-<!--        :zoom="12"-->
-<!--  />-->
-<!--  <v-container>-->
-<!--    <v-layout-->
-<!--        text-center-->
-<!--        wrap-->
-<!--    >-->
-<!--      <v-flex xs12>-->
-<!--        <v-img-->
-<!--            :src="require('../assets/logo_dave.png')"-->
-<!--            class="my-3"-->
-<!--            contain-->
-<!--            height="200"-->
-<!--        />-->
-<!--      </v-flex>-->
-
-<!--      <v-flex mb-4>-->
-<!--        <h1 class="display-2 font-weight-bold mb-3">-->
-<!--          Willkommen bei DAVe-->
-<!--        </h1>-->
-<!--        <h2>-->
-<!--          Datenbank und Auswertungen von Verkehrszählungen-->
-<!--        </h2>-->
-<!--        <v-btn-->
-<!--            id="chart-demo-btn"-->
-<!--            :to="{path: '/chartdemo'}"-->
-<!--        >-->
-<!--          <v-icon>mdi-google-downasaur</v-icon>-->
-<!--        </v-btn>-->
-<!--      </v-flex>-->
-<!--    </v-layout>-->
-<!--  </v-container>-->
 </template>
 
 <script lang="ts">
