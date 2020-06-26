38c7a315d28bf8ac5b1a32efab88fcac3756b471
xdoo <claus.straube@gmail.com>
2020-06-26 09:22:06 +0200

    some imporvements on todo list

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -9,18 +9,18 @@
       outlined
     ></v-text-field>
     <v-list two-line subheader nav class="pb-10">
-      <v-subheader inset>Zählungen in Bearbeitung</v-subheader>
+      <v-subheader inset>geplante Zählungen</v-subheader>
       <v-list-item
         to="foo"
-      >
+      > 
         <v-list-item-avatar
           color="blue lighten-4"
-        >
+        >   
           <v-icon>mdi-calendar-plus</v-icon>
         </v-list-item-avatar>
 
         <v-list-item-content>
-          <v-list-item-title>Zählung vom 20.08.2020 [geplant]</v-list-item-title>
+          <v-list-item-title><v-icon color="red">mdi-new-box</v-icon> Zählung vom 20.08.2020 | Dachauerstr. / Hugotrondlestr.</v-list-item-title>
           <v-list-item-subtitle>Wegen Veränderung der Ampelschaltung.</v-list-item-subtitle>
         </v-list-item-content>
 
@@ -34,19 +34,70 @@
         to="foo"
       >
         <v-list-item-avatar
+          color="blue lighten-4"
+        >
+          <v-icon>mdi-calendar-plus</v-icon>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>Zählung vom 26.08.2020 | Königsplatz</v-list-item-title>
+          <v-list-item-subtitle>Wegen Veränderung der Ampelschaltung.</v-list-item-subtitle>
+        </v-list-item-content>
+
+        <v-list-item-action>
+          <v-btn icon>
+            <v-icon color="grey lighten-1">mdi-email-send-outline</v-icon>
+          </v-btn>
+        </v-list-item-action>
+      </v-list-item>
+      <v-list-item
+        to="foo"
+      >
+        <v-list-item-avatar
+          color="blue lighten-4"
+        >
+          <v-icon>mdi-calendar-plus</v-icon>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>Zählung vom 04.09.2020 | Prinzregentenstr.</v-list-item-title>
+          <v-list-item-subtitle>Wegen Veränderung der Ampelschaltung.</v-list-item-subtitle>
+        </v-list-item-content>
+
+        <v-list-item-action>
+          <v-btn icon>
+            <v-icon color="grey lighten-1">mdi-email-send-outline</v-icon>
+          </v-btn>
+        </v-list-item-action>
+      </v-list-item>
+
+
+      <v-divider inset></v-divider>
+      <v-subheader inset>beauftragte Zählungen</v-subheader>
+      <v-list-item
+        to="foo"
+      >
+        <v-list-item-avatar
           color="red lighten-4"
         >
           <v-icon>mdi-calendar-refresh</v-icon>
         </v-list-item-avatar>
 
         <v-list-item-content>
-          <v-list-item-title>Zählung vom 25.06.2020 [beauftragt]</v-list-item-title>
+          <v-list-item-title>Zählung vom 03.07.2020 | Vollmannstr. / Englschalkinger Str.</v-list-item-title>
           <v-list-item-subtitle>Wegen Umbau der Kreuzung.</v-list-item-subtitle>
         </v-list-item-content>
 
         <v-list-item-action>
+          <v-btn icon>
+            <v-icon color="grey lighten-1">mdi-information</v-icon>
+          </v-btn>
         </v-list-item-action>
       </v-list-item>
+
+
+      <v-divider inset></v-divider>
+      <v-subheader inset>durchgeführte Zählungen</v-subheader>
       <v-list-item
         to="foo"
       >
@@ -64,170 +115,6 @@
         <v-list-item-action>
           <v-btn icon>
             <v-icon color="grey lighten-1">mdi-earth-plus</v-icon>
-          </v-btn>
-        </v-list-item-action>
-      </v-list-item>
-
-      <v-divider inset></v-divider>
-      <v-subheader inset>Veröffentlichte Zählungen</v-subheader>
-
-      <v-list-item
-        to="foo"
-      >
-        <v-list-item-avatar
-          color="grey lighten-2"
-        >
-          <v-icon>mdi-chart-bar</v-icon>
-        </v-list-item-avatar>
-
-        <v-list-item-content>
-          <v-list-item-title>Zählung vom 14.12.2019</v-list-item-title>
-          <v-list-item-subtitle>Wegen Veränderung der Ampelschaltung.</v-list-item-subtitle>
-        </v-list-item-content>
-
-        <v-list-item-action>
-          <v-btn icon>
-            <v-icon color="grey lighten-1">mdi-information</v-icon>
-          </v-btn>
-        </v-list-item-action>
-      </v-list-item>
-      <v-list-item
-        to="foo"
-      >
-        <v-list-item-avatar
-          color="grey lighten-2"
-        >
-          <v-icon>mdi-chart-bar</v-icon>
-        </v-list-item-avatar>
-
-        <v-list-item-content>
-          <v-list-item-title>Zählung vom 14.12.2018</v-list-item-title>
-          <v-list-item-subtitle>Wegen Veränderung der Ampelschaltung.</v-list-item-subtitle>
-        </v-list-item-content>
-
-        <v-list-item-action>
-          <v-btn icon>
-            <v-icon color="grey lighten-1">mdi-information</v-icon>
-          </v-btn>
-        </v-list-item-action>
-      </v-list-item>
-      <v-list-item
-        to="foo"
-      >
-        <v-list-item-avatar
-          color="grey lighten-2"
-        >
-          <v-icon>mdi-chart-bar</v-icon>
-        </v-list-item-avatar>
-
-        <v-list-item-content>
-          <v-list-item-title>Zählung vom 14.12.2016</v-list-item-title>
-          <v-list-item-subtitle>Wegen Veränderung der Ampelschaltung.</v-list-item-subtitle>
-        </v-list-item-content>
-
-        <v-list-item-action>
-          <v-btn icon>
-            <v-icon color="grey lighten-1">mdi-information</v-icon>
-          </v-btn>
-        </v-list-item-action>
-      </v-list-item>
-      <v-list-item
-        to="foo"
-      >
-        <v-list-item-avatar
-          color="grey lighten-2"
-        >
-          <v-icon>mdi-chart-bar</v-icon>
-        </v-list-item-avatar>
-
-        <v-list-item-content>
-          <v-list-item-title>Zählung vom 14.12.2013</v-list-item-title>
-          <v-list-item-subtitle>Wegen Veränderung der Ampelschaltung.</v-list-item-subtitle>
-        </v-list-item-content>
-
-        <v-list-item-action>
-          <v-btn icon>
-            <v-icon color="grey lighten-1">mdi-information</v-icon>
-          </v-btn>
-        </v-list-item-action>
-      </v-list-item>
-      <v-list-item
-        to="foo"
-      >
-        <v-list-item-avatar
-          color="grey lighten-2"
-        >
-          <v-icon>mdi-chart-bar</v-icon>
-        </v-list-item-avatar>
-
-        <v-list-item-content>
-          <v-list-item-title>Zählung vom 14.12.2011</v-list-item-title>
-          <v-list-item-subtitle>Wegen Veränderung der Ampelschaltung.</v-list-item-subtitle>
-        </v-list-item-content>
-
-        <v-list-item-action>
-          <v-btn icon>
-            <v-icon color="grey lighten-1">mdi-information</v-icon>
-          </v-btn>
-        </v-list-item-action>
-      </v-list-item>
-      <v-list-item
-        to="foo"
-      >
-        <v-list-item-avatar
-          color="grey lighten-2"
-        >
-          <v-icon>mdi-chart-bar</v-icon>
-        </v-list-item-avatar>
-
-        <v-list-item-content>
-          <v-list-item-title>Zählung vom 14.12.2006</v-list-item-title>
-          <v-list-item-subtitle>Wegen Veränderung der Ampelschaltung.</v-list-item-subtitle>
-        </v-list-item-content>
-
-        <v-list-item-action>
-          <v-btn icon>
-            <v-icon color="grey lighten-1">mdi-information</v-icon>
-          </v-btn>
-        </v-list-item-action>
-      </v-list-item>
-      <v-list-item
-        to="foo"
-      >
-        <v-list-item-avatar
-          color="grey lighten-2"
-        >
-          <v-icon>mdi-chart-bar</v-icon>
-        </v-list-item-avatar>
-
-        <v-list-item-content>
-          <v-list-item-title>Zählung vom 14.12.2002</v-list-item-title>
-          <v-list-item-subtitle>Wegen Veränderung der Ampelschaltung.</v-list-item-subtitle>
-        </v-list-item-content>
-
-        <v-list-item-action>
-          <v-btn icon>
-            <v-icon color="grey lighten-1">mdi-information</v-icon>
-          </v-btn>
-        </v-list-item-action>
-      </v-list-item>
-      <v-list-item
-        to="foo"
-      >
-        <v-list-item-avatar
-          color="grey lighten-2"
-        >
-          <v-icon>mdi-chart-bar</v-icon>
-        </v-list-item-avatar>
-
-        <v-list-item-content>
-          <v-list-item-title>Zählung vom 14.12.1994</v-list-item-title>
-          <v-list-item-subtitle>Wegen Veränderung der Ampelschaltung.</v-list-item-subtitle>
-        </v-list-item-content>
-
-        <v-list-item-action>
-          <v-btn icon>
-            <v-icon color="grey lighten-1">mdi-information</v-icon>
           </v-btn>
         </v-list-item-action>
       </v-list-item>
